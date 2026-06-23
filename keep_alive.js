e2b493bd21805d3fbc886acb805826d89fb42036
Aaron Bobby <bobbyaaron456@gmail.com>
2026-06-23 15:32:09 +0530

    fix: render fix

--- keep_alive.js
+++ keep_alive.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.all('/', (req, res) => {
+app.all('*', (req, res) => {
     res.send('Bot is running!');
 });
 
 function keepAlive() {
-    app.listen(3000, () => {
-        console.log('Server is ready. Keep-alive active.');
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server is ready on port ${port}. Keep-alive active.`);
     });
 }
 
